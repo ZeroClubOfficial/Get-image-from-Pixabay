38ef52bffebfe27c63cb3cd0595fc426077691f3
Tran Huu Dang <tranhuudang148@gmail.com>
2022-03-07 15:33:43 +0700

    Add project files.

---
+++ Get image from Pixabay/mainHall.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Get_image_from_Pixabay
+{
+    partial class mainHall
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.word = new System.Windows.Forms.TextBox();
+            this.btGet = new System.Windows.Forms.Button();
+            this.picture = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // word
+            // 
+            this.word.Location = new System.Drawing.Point(12, 483);
+            this.word.Name = "word";
+            this.word.Size = new System.Drawing.Size(466, 20);
+            this.word.TabIndex = 0;
+            // 
+            // btGet
+            // 
+            this.btGet.Location = new System.Drawing.Point(484, 481);
+            this.btGet.Name = "btGet";
+            this.btGet.Size = new System.Drawing.Size(75, 23);
+            this.btGet.TabIndex = 1;
+            this.btGet.Text = "Get";
+            this.btGet.UseVisualStyleBackColor = true;
+            this.btGet.Click += new System.EventHandler(this.btGet_Click);
+            // 
+            // picture
+            // 
+            this.picture.ImageLocation = "";
+            this.picture.Location = new System.Drawing.Point(12, 12);
+            this.picture.Name = "picture";
+            this.picture.Size = new System.Drawing.Size(547, 400);
+            this.picture.TabIndex = 2;
+            this.picture.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 418);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(547, 57);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "You can use your own key API in this link bellow\nhttps://pixabay.com/api/docs/";
+            // 
+            // mainHall
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 516);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.picture);
+            this.Controls.Add(this.btGet);
+            this.Controls.Add(this.word);
+            this.Name = "mainHall";
+            this.ShowIcon = false;
+            this.Text = "Pixabay Picture";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox word;
+        private System.Windows.Forms.Button btGet;
+        private System.Windows.Forms.PictureBox picture;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Get image from Pixabay/mainHall.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.Json.Nodes;
+
+
+namespace Get_image_from_Pixabay
+{
+    public partial class mainHall : Form
+    {
+        public mainHall()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //https://pixabay.com/api/?key=25829393-af32bf17ec8386b5941fb5f8f&q=yellow+flowers&image_type=photo
+            //25829393-af32bf17ec8386b5941fb5f8f
+
+
+        }
+
+        private async void btGet_Click(object sender, EventArgs e)
+        {
+            string rawContent;
+            string imageUrl;
+            HttpClient client = new HttpClient();
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, @"https://pixabay.com/api/?key=25829393-af32bf17ec8386b5941fb5f8f&q="+word.Text+@"&image_type=photo");
+            using (var responseMessage = await client.SendAsync(request))
+            {
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    rawContent= await responseMessage.Content.ReadAsStringAsync();
+                    JsonNode jsonContent = JsonNode.Parse(rawContent);
+                    imageUrl = jsonContent["hits"][0]["webformatURL"].GetValue<string>();
+                    picture.ImageLocation = imageUrl;
+                }
+                   
+            }
+        }
+    }
+}
